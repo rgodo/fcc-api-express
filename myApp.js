e861aceb2554fc4d5784579757ee81ba55f56b48
Rodrigo Godo <31048484+rgodo@users.noreply.github.com>
2021-08-23 17:58:16 +0000

    se devuelve index.html,  middleware para public y ruta json

--- myApp.js
+++ myApp.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var app = express();
 
+//console.log('Hello world');
+
+app.use('/public', express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/json', (req, res) => {
+  res.json({"message" : "Hello json"});
+});
+
+
+
 
 
 
